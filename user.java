e2bbfe2c9fec9a2f1d43c4b16127135ad38b25cb
jeevithajeevitha92294-dot <jeevithajeevitha92294@gmail.com>
2026-07-28 23:43:39 +0530

    Create user.java

---
+++ user.java
@@ -0,0 +1,17 @@
+package model;
+
+public class User {
+
+   public String name;
+   public String email;
+   public String password;
+   public String role;
+
+    public User(String name, String email, String password, String role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+}

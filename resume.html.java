ff9609170349a8a4f03fde4286c6590781e20153
jeevithajeevitha92294-dot <jeevithajeevitha92294@gmail.com>
2026-07-28 23:41:54 +0530

    Create resume.html.java

---
+++ resume.html.java
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Resume Builder - NextHire</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<div class="dashboard-box">
+
+    <h1>📄 Resume Builder</h1>
+    <hr>
+
+    <h3>Create Your Professional Resume</h3>
+
+    <p>
+        
+    </p>
+
+    <br>
+
+    <button onclick="window.location.href='generated_resume.html'">
+        📄 Generate Professional Resume
+    </button>
+
+    <br><br>
+
+    <button onclick="window.location.href='dashboard.html'">
+        ⬅ Back to Dashboard
+    </button>
+
+</div>
+
+</body>
+</html>

0f9088bda5d4188d0cb97b8c3f0ca42fe2f19a87
jeevithajeevitha92294-dot <jeevithajeevitha92294@gmail.com>
2026-07-28 23:47:53 +0530

    Create test register.java

---
+++ test register.java
@@ -0,0 +1,17 @@
+public class TestRegister {
+
+    public static void main(String[] args) {
+
+        Register.registerUser(
+            "Albena",
+            "albena@gmail.com",
+            "12345",
+            "student",
+            "Dhaanish Ahmed College of Engineering",
+            "B.Tech AI & DS",
+            "Java, Python, SQL",
+            "Artificial Intelligence"
+        );
+
+    }
+}
